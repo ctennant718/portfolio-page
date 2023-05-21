ac3146ca5289f292497bcbd66ee286e6de1bcabc
Charlotte Tennant <80706604+ctennant718@users.noreply.github.com>
2023-05-21 16:51:58 +0100

    intro component built

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,13 +1,48 @@
-import React from 'react'
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import Charlotte from "../images/charlotte-image.jpeg";
 
 function Intro() {
   return (
     <>
-      <section>
-        
+      <section className="intro-section">
+        <div className="intro-image-wrapper">
+          <img
+            className="intro-image"
+            src={Charlotte}
+            alt="Charlotte has shoulder-length brown hair and wears glasses. She is smiling for the camera."
+          />
+        </div>
+        <div className="intro-text-wrapper">
+          <h1 className="intro-headline">
+            Hi! My name is Charlotte
+          </h1>
+          <p className="intro-text">
+            I am software engineer specialising in JavaScript, React and Node.
+          </p>
+          <p className="intro-text">
+            I couple this with proven project management and communication
+            skills.
+          </p>
+          <div className="intro-links-wrapper">
+            <a
+              className="intro-link"
+              href="https://www.linkedin.com/in/charlotte-tennant-141b37274/"
+            >
+              &#x2022; <span className="intro-link-individual">LinkedIn</span>{" "}
+              &#x2022;{" "}
+            </a>
+            <a className="intro-link" href="https://github.com/ctennant718">
+              <span className="intro-link-individual">GitHub</span> &#x2022;{" "}
+            </a>
+            <a className="intro-link" href="">
+              <span className="intro-link-individual">CV</span> &#x2022;
+            </a>
+          </div>
+        </div>
       </section>
     </>
-  )
+  );
 }
 
-export default Intro
+export default Intro;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import Header from "../components/Header";
+import Intro from "../components/Intro";
 import "../styles/main.css";
 
 function Home() {
   return (
     <>
       <Header />
+      <Intro />
     </>
   );
 }
