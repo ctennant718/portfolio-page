540f7c4f9af5ff752c6f580360fc53079b222d97
Charlotte Tennant <80706604+ctennant718@users.noreply.github.com>
2023-06-16 15:42:11 +0100

    images added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,6 +9,12 @@ import "../styles/main.css";
 //images
 import shopfrontImage from "../images/shopfront-project.jpeg";
 import hangman from "../images/hangman.png";
+import hangmanImage from "../images/hangman-project.png";
+import guideDogsImage from "../images/guide-dogs-project.png";
+import fourCardImage from "../images/four-card-project.png";
+import animalCrossingImage from "../images/animal-crossing-project.png";
+import chocolateImage from "../images/chocolate-project.png";
+import portfolioPageImage from "../images/portfolio-page.png"
 
 function Home() {
   return (
@@ -25,9 +31,9 @@ function Home() {
         />
         <ProjectCard
           projectTitle="Hangman"
-          projectImage={hangman}
+          projectImage={hangmanImage}
           projectUrl="https://typescript-hangman-ochre.vercel.app/"
-          projectDescription="The familiar word game, created using React and TypeScript. Can you guess the word?"
+          projectDescription="The familiar word game, created using React (with hooks and conditional rendering) and TypeScript. Can you guess the word in time?"
           gitHubUrl="https://github.com/ctennant718/typescript-hangman"
         />
         <ProjectCard
@@ -39,7 +45,7 @@ function Home() {
         />
         <ProjectCard
           projectTitle="Guide Dogs homage"
-          projectImage={hangman}
+          projectImage={guideDogsImage}
           projectUrl="https://meek-sunshine-1acdae.netlify.app/"
           projectDescription="A homage to the Guide Dogs website whose wonderful work I have been lucky enough to see first hand. Built using semantically valid and accessible HTML, and SCSS."
           gitHubUrl="https://github.com/ctennant718/guide-dogs-homage"
@@ -48,43 +54,43 @@ function Home() {
           projectTitle="Cats vs Dogs"
           projectImage={hangman}
           projectUrl="https://lambent-kelpie-2c9984.netlify.app/"
-          projectDescription="A homage to the Guide Dogs website whose wonderful work I have been lucky enough to see first hand. Built using semantically valid and accessible HTML, and SCSS."
+          projectDescription="Vote on the ultimate question: are you a cat or a dog person? Realtime updates, created using JavaScript and Firebase (particularly Firestore)."
           gitHubUrl="https://github.com/ctennant718/firebase-cats-dogs"
         />
-        {/* <ProjectCard
-          projectTitle="Sweet tooth"
-          projectImage={hangman}
-          projectUrl="https://sage-tartufo-7e30ee.netlify.app/"
-          projectDescription="A quick Chart.js project - a simple way to visualise data using this handy JavaScript library."
-          gitHubUrl="https://github.com/ctennant718/chart-js-chocolate"
-        /> */}
         <ProjectCard
           projectTitle="Four-card feature"
-          projectImage={hangman}
+          projectImage={fourCardImage}
           projectUrl="https://harmonious-snickerdoodle-0c5d6e.netlify.app/"
-          projectDescription="Created by following Figma design files, this was a great, responsive design to build from the resource Frontend Mentor."
+          projectDescription="Created by following Figma design files, this responsive design was built from a challenge set by the amazing resource Frontend Mentor."
           gitHubUrl="https://github.com/ctennant718/four-card-feature"
         />
         <ProjectCard
           projectTitle="My island"
-          projectImage={hangman}
+          projectImage={animalCrossingImage}
           projectUrl="https://peppy-duckanoo-6f0e22.netlify.app/"
-          projectDescription="Experimenting with the Animal Crossing REST API to draw in a list of the villagers featured in the game. Who is your favourite? "
+          projectDescription="Experimenting with the Animal Crossing REST API to display the villagers I have on my island. Who is your favourite? "
           gitHubUrl="https://github.com/ctennant718/animal-crossing-api"
         />
         <ProjectCard
           projectTitle="Good dog"
           projectImage={hangman}
           projectUrl="#"
-          projectDescription="An API of good boys built using Node, Express and MongoDB/Mongoose. Check out the Github link!"
+          projectDescription="An API of good boys built using Node, Express and MongoDB/Mongoose. Check out the Github repo below!"
           gitHubUrl="https://github.com/ctennant718/dogs-api"
         />
         <ProjectCard
           projectTitle="Portfolio page"
-          projectImage={hangman}
-          projectUrl="#"
+          projectImage={portfolioPageImage}
+          projectUrl=""
           projectDescription="This page was built using React, with React Router. I was also keen to practice my CSS skills instead of using a framework."
           gitHubUrl="https://github.com/ctennant718/portfolio-page"
+        />
+        <ProjectCard
+          projectTitle="Sweet tooth"
+          projectImage={chocolateImage}
+          projectUrl="https://sage-tartufo-7e30ee.netlify.app/"
+          projectDescription="A quick Chart.js project - a simple way to visualise data using this handy JavaScript library."
+          gitHubUrl="https://github.com/ctennant718/chart-js-chocolate"
         />
       </div>
     </>
