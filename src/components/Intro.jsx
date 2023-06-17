bb98a7de35f5eff9ce12e1351dc6d884622a05ad
Charlotte Tennant <80706604+ctennant718@users.noreply.github.com>
2023-06-17 15:23:42 +0100

    cv added

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -6,22 +6,22 @@ function Intro() {
   return (
     <>
       <section className="intro-section">
-          <div className="intro-image-wrapper">
-            <img
-              className="intro-image"
-              src={Charlotte}
-              alt="Charlotte has shoulder-length brown hair and wears glasses. She is smiling for the camera."
-            />
-          </div>
-          <div className="intro-text-wrapper">
-            <h1 className="intro-headline">Hi! I&apos;m Charlotte</h1>
-            <p className="intro-text">
-              I am a software engineer specialising in JavaScript, React and Node.
-            </p>
-            <p className="intro-text">
-              I couple this with proven project management and communication
-              skills.
-            </p>
+        <div className="intro-image-wrapper">
+          <img
+            className="intro-image"
+            src={Charlotte}
+            alt="Charlotte has shoulder-length brown hair and wears glasses. She is smiling for the camera."
+          />
+        </div>
+        <div className="intro-text-wrapper">
+          <h1 className="intro-headline">Hi! I&apos;m Charlotte</h1>
+          <p className="intro-text">
+            I am a software engineer specialising in JavaScript, React and Node.
+          </p>
+          <p className="intro-text">
+            I couple this with proven project management and communication
+            skills.
+          </p>
           <div className="intro-links-wrapper">
             <a
               className="intro-link"
@@ -34,7 +34,7 @@ function Intro() {
             <a className="intro-link" href="https://github.com/ctennant718">
               <span className="intro-link-individual git-hub-link">GitHub</span>
             </a>
-            <a className="intro-link" href="src/charlotte-tennant.pdf" download>
+            <a className="intro-link" href="src/assets/charlottetennant.pdf" download>
               <span className="intro-link-individual cv-link">CV</span>
             </a>
           </div>
